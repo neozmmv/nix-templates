b9a099fbf9206f8d84ff4b731a78e7b55df1d797
Enzo Gutiérrez Pereira <83484706+neozmmv@users.noreply.github.com>
2026-05-29 20:01:04 -0300

    Add basic flake for development shells

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A very basic flake for development shells";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            hello
+            cowsay
+          ];
+        };
+      }
+    );
+}
